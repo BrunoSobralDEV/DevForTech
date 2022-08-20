742c844dc48ce8be435562450f584affb6d63355
Bruno Sobral <brunosobralss@hotmail.com>
2022-08-20 15:40:31 -0300

    feat: getElements

---
+++ calculator/assets/scripts.js
@@ -0,0 +1,18 @@
+const $ = (query) => document.querySelector(query);
+
+const btn1 = $('#btn1').innerHTML
+const btn2 = $('#btn2').innerHTML
+const btn3 = $('#btn3').innerHTML
+const btn4 = $('#btn4').innerHTML
+const btn5 = $('#btn5').innerHTML
+const btn6 = $('#btn6').innerHTML
+const btn7 = $('#btn7').innerHTML
+const btn8 = $('#btn8').innerHTML
+const btn9 = $('#btn9').innerHTML
+const btnSum = $('#btn9').innerHTML
+const btnSubtraction = $('#btn9').innerHTML
+const btnMultiplication = $('#btn9').innerHTML
+const btnDivision = $('#btn9').innerHTML
+const btnEqual = $('#btn9').innerHTML
+
+console.log(btn3)
