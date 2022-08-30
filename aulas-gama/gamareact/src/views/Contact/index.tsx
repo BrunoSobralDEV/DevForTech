664acb3eafdee899bc62aecccb4c38ef2289a178
Bruno Sobral <brunosobralss@hotmail.com>
2022-08-30 17:08:08 -0300

    feat/useState for inputs

--- aulas-gama/gamareact/src/views/Contact/index.tsx
+++ aulas-gama/gamareact/src/views/Contact/index.tsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { Container } from "./style";
+
+interface IData {
+  name: string;
+  email: string;
+}
+
 export function Contact() {
+  const [data, setData] = useState<IData>({} as IData);
+
   return (
-    <h1>Contato</h1>
+    <Container>
+      <div className="form-wrapper">
+        <h1>Contato</h1>
+        Nome: {data.name}{" "}
+        E-mail: {data.email}
+        <div className="card">
+          <form onSubmit={() => {}}>
+            <input 
+              type="text" 
+              placeholder="Nome" 
+              onChange={e=> setData({...data, name: e.target.value})}
+            />
+            <input 
+              type="text" 
+              placeholder="E-mail" 
+              onChange={e=> setData({...data, email: e.target.value})}
+            />
+            <input 
+              type="submit" 
+              value="Cadastrar" 
+            />
+          </form>
+        </div>
+
+        <Link to='/'>Voltar para Home</Link>
+      </div>
+    </Container>
   )
 };
--- aulas-gama/gamareact/src/views/Home/style.ts
+++ aulas-gama/gamareact/src/views/Home/style.ts
@@ -2,7 +2,14 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 100vh;
   background: green;
+  color: #fff;
+
+  .button-container {
+    display: block;
+  }
 `;
